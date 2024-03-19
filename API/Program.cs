7623c71067d4d18cb53caa794c4ad71ee7c2815d
Felipepupo22 <felipepupo53@gmail>
2024-03-18 21:41:32 -0300

    Aula 18/03/24

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<Produto> produtos = new List<Produto>()
+{
+    new Produto("Celular", "Android"),
+    new Produto("Celular", "IOS"),
+    new Produto("Celular", "LG")
+};
+
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/API/produto", () => produtos);
+
 app.Run();
+
+public record Produto(string Nome, string Descricao);
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2b2f67a570dd4c5fa884cc41a032aa633f5247")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
